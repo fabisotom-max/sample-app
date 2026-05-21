2f236b621600e342ecc6d22f77821848704d2933
fabisotom-max <fabi.sotom@duocuc.cl>
2026-05-21 02:23:07 +0000

    Fix script append error y version python

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
+rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python:3.8" >> tempdir/Dockerfile
+echo "FROM python:3.8" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 9999" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
